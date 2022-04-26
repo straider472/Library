13eef09a47f8bed89baf8e97b94a37571f897e8b
Овчинников Илья <ilya_ovchinnikov_1999@list.ru>
2022-04-26 14:16:04 +0500

    Мелкие правки с логикой работы

--- Library/Pages/Add_Order.xaml.cs
+++ Library/Pages/Add_Order.xaml.cs
@@ -25,18 +25,29 @@ namespace Library.Pages
             InitializeComponent();
             //Привязка данных
             dt_books.ItemsSource = Model.LibraryEntities.GetContext().Books.ToList();
+            cb_readers.ItemsSource = Model.LibraryEntities.GetContext().Readers.ToList();
         }
+        //Список книг
+        public static List<Model.Books> list_books = new List<Model.Books>();
 
         //Выбор книги
         private void Select_Book(object sender, RoutedEventArgs e)
         {
-
+            if (list_books.Count < 3) list_books.Add(dt_books.SelectedItem as Model.Books);
+            else
+            {
+                MessageBox.Show("Количество книг не должно быть более трёх!");
+                list_books.Remove(dt_books.SelectedItem as Model.Books);
+                count_books.Content = list_books.Count.ToString();
+            }
+            count_books.Content = list_books.Count.ToString();
         }
 
         //Удаление книги
         private void Delete_Book(object sender, RoutedEventArgs e)
         {
-
+            list_books.Remove(dt_books.SelectedItem as Model.Books);
+            count_books.Content = list_books.Count.ToString();
         }
     }
 }
